d66b668dde46b269a032911b6e7d5b36f402ffd6
黃禹翔 <hyusterr>
2021-12-07 02:14:49 +0800

    finish read_xml in utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@
 # TODO: familiar with git
 import sys
 import xml.etree.ElementTree as ET
-# import xmltodict
+from pprint import pprint
 
 # think point: XML information may highlight something, e.g. bold means important
 def read_xml(filename):
@@ -23,14 +23,13 @@ def read_xml(filename):
 
     tree = ET.parse(filename)
     root = tree.getroot()
+    output = {child.tag: ET.tostring(child, encoding='utf-8', method='text').decode("utf-8") for child in root}
 
-    for child in root:
-        print(child.tag)
-        print(ET.tostring(child, encoding='utf-8', method='text'))
+    return output
 
 
 if __name__ == '__main__':
-    read_xml(sys.argv[1])
+    pprint(read_xml(sys.argv[1]))
 
 
 
