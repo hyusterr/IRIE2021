c98a506648b01f933c295d5e90390b8a7feff780
黃禹翔 <hyusterr>
2021-12-07 02:55:52 +0800

    train_query/11 has XML formatting issue

--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@
 import sys
 import xml.etree.ElementTree as ET
 from pprint import pprint
+from pathlib import Path
 
 # think point: XML information may highlight something, e.g. bold means important
 def read_xml(filename):
@@ -28,8 +29,37 @@ def read_xml(filename):
     return output
 
 
+def chunk_into_pieces(directory):
+    '''
+    chunk files from doc/train_query/test_query into pieces and save into assigned directory
+    input:
+    - directory: str
+
+    output:
+    - None
+    - saving logistic:
+        - e.g. doc/3052048 contains file with abstract and body
+          save each part to
+          doc/abstract/3052048
+          doc/body/3052048
+    '''
+    
+    parent_path = Path(directory)
+    for child in parent_path.iterdir():
+        if child.is_file():
+            content_dict = read_xml(child)
+            for name, content in content_dict.items():
+                save_dir = parent_path / name
+                if not save_dir.exists():
+                    save_dir.mkdir()
+                filename = child.name
+                save_path = save_dir / filename
+                save_path.write_text(content.strip(), encoding='utf-8')
+
+
+
 if __name__ == '__main__':
-    pprint(read_xml(sys.argv[1]))
+    chunk_into_pieces(sys.argv[1])
 
 
 
