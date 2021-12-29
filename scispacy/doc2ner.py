110fd9a3acacdb0391e1047d94c67ab66d7e220c
黃禹翔 <hyusterr>
2021-12-30 01:05:15 +0800

    test en_core_sci_scibert

--- scispacy/doc2ner.py
+++ scispacy/doc2ner.py
@@ -1,14 +1,22 @@
 #!usr/bin/python3
 # encoding: utf-8
 
+import sys
 import click
 import spacy
 from scispacy.abbreviation import AbbreviationDetector
 
 spacy.prefer_gpu()
 
-nlp = spacy.load("en_core_sci_sm")
+# en_core_sci_sm performs already well
+nlp = spacy.load(sys.argv[2])
 # Add the abbreviation pipe to the spacy pipeline.
 nlp.add_pipe("abbreviation_detector")
 
+def doc2ner(text: str) -> str:
+    doc = nlp(text)
+    print(" ".join([ent.text for ent in doc.ents]))
 
+with open(sys.argv[1], 'r') as f:
+    text = f.read()
+doc2ner(text)
