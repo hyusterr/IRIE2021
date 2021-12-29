83cffb25055197906e9f28b3a1ae5e46551c0fd2
黃禹翔 <hyusterr>
2021-12-30 01:39:25 +0800

    add pathlib

--- scispacy/doc2ner.py
+++ scispacy/doc2ner.py
@@ -4,19 +4,37 @@
 import sys
 import click
 import spacy
+from pathlib import Path
+from tqdm.auto import tqdm
 from scispacy.abbreviation import AbbreviationDetector
 
 spacy.prefer_gpu()
 
 # en_core_sci_sm performs already well
-nlp = spacy.load(sys.argv[2])
+nlp = spacy.load("en_core_sci_sm")
 # Add the abbreviation pipe to the spacy pipeline.
-nlp.add_pipe("abbreviation_detector")
+# nlp.add_pipe("abbreviation_detector")
 
-def doc2ner(text: str) -> str:
+def doc2ner(text: str, nlp=nlp) -> str:
     doc = nlp(text)
-    print(" ".join([ent.text for ent in doc.ents]))
+    ners = " ".join([ent.text for ent in doc.ents])
+    # print(" ".join([abrv._.long_form for abrv in doc._.abbreviations]))
+    return ners
 
-with open(sys.argv[1], 'r') as f:
-    text = f.read()
-doc2ner(text)
+
+def main(directory):
+    parent_path = Path(directory)
+    for child in tqdm(parent_path.iterdir()):
+        if child.is_file():
+            with child.open() as f: text = f.read()
+            ner_text = doc2ner(text)
+            save_dir = parent_path / 'NER'
+            if not save_dir.exists():
+                save_dir.mkdir()
+            filename = child.name
+            save_path = save_dir / filename
+            save_path.write_text(ner_text.strip(), encoding='utf-8')
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
